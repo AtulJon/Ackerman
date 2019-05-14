65c6f3dc403014e911b7654e0edd5c72023eae77
AtulJon <50616240+AtulJon@users.noreply.github.com>
2019-05-14 16:07:13 +0530

    Update voc.py

--- voc.py
+++ voc.py
@@ -1,5 +1,8 @@
-k=(input"")
-if k in ('a','e','i','o','u'):
-	print("Vowel")
+str=(input(""))
+if str.isalpha() and len(str)==1:
+	if str in ('a','e','i','o','u') or str in ('A','E','I','O','U'):
+		print("Vowel")
+	else:
+		print("Consonant")
 else:
-	print("Consonant")
+	print("Inalid")
